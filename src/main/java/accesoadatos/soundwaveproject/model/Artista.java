ba8953a3ea66f5c244ab8c74381b032fe2fd31e5
Jose_Luis <joselu2004.jlrc@gmail.com>
2023-10-30 20:22:13 +0100

    model artista y disco

--- src/main/java/accesoadatos/soundwaveproject/model/Artista.java
+++ src/main/java/accesoadatos/soundwaveproject/model/Artista.java
@@ -1,4 +1,66 @@
 package accesoadatos.soundwaveproject.model;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Artista {
+   private String dni;
+   private String nombre;
+   private String nacionalidad;
+   private byte [] foto;
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public byte[] getFoto() {
+        return foto;
+    }
+
+    public void setFoto(byte[] foto) {
+        this.foto = foto;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Artista artista = (Artista) o;
+        return dni.equals(artista.dni);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dni);
+    }
+
+    @Override
+    public String toString() {
+        return "Artista{" +
+                "dni='" + dni + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", nacionalidad='" + nacionalidad + '\'' +
+                ", foto=" + Arrays.toString(foto) +
+                '}';
+    }
 }
--- src/main/java/accesoadatos/soundwaveproject/model/Disco.java
+++ src/main/java/accesoadatos/soundwaveproject/model/Disco.java
@@ -1,4 +1,77 @@
 package accesoadatos.soundwaveproject.model;
 
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Disco {
+    private String dni;
+    private String nombre;
+    private LocalTime fechaPublicacion;
+    private byte [] foto;
+    private String reproducion;
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalTime getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+
+    public void setFechaPublicacion(LocalTime fechaPublicacion) {
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
+    public byte[] getFoto() {
+        return foto;
+    }
+
+    public void setFoto(byte[] foto) {
+        this.foto = foto;
+    }
+
+    public String getReproducion() {
+        return reproducion;
+    }
+
+    public void setReproducion(String reproducion) {
+        this.reproducion = reproducion;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Disco disco = (Disco) o;
+        return dni.equals(disco.dni);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dni);
+    }
+
+    @Override
+    public String toString() {
+        return "Disco{" +
+                "dni='" + dni + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", fechaPublicacion=" + fechaPublicacion +
+                ", foto=" + Arrays.toString(foto) +
+                ", reproducion='" + reproducion + '\'' +
+                '}';
+    }
 }
