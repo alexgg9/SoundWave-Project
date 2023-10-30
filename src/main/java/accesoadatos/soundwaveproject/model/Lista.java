5f3b1917da1e914bed7aa0b8d1efb14ebaaeeffe
RSAdrian9 <adrian4rs@gmail.com>
2023-10-30 20:39:49 +0100

    Cambios Modelo

--- src/main/java/accesoadatos/soundwaveproject/model/Lista.java
+++ src/main/java/accesoadatos/soundwaveproject/model/Lista.java
@@ -1,4 +1,120 @@
 package accesoadatos.soundwaveproject.model;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Lista {
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private String dni_usuario;
+    private int suscripciones;
+    Usuario creados;
+    List<Comentario> comentarios;
+    List<Usuario> seguidores;
+
+    public Lista() {
+    }
+
+    public Lista(int id, String nombre, String descripcion, String dni_usuario, int suscripciones, Usuario creados, List<Comentario> comentarios, List<Usuario> seguidores) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.dni_usuario = dni_usuario;
+        this.suscripciones = suscripciones;
+        this.creados = creados;
+        this.comentarios = comentarios;
+        this.seguidores = seguidores;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDni_usuario() {
+        return dni_usuario;
+    }
+
+    public void setDni_usuario(String dni_usuario) {
+        this.dni_usuario = dni_usuario;
+    }
+
+    public int getSuscripciones() {
+        return suscripciones;
+    }
+
+    public void setSuscripciones(int suscripciones) {
+        this.suscripciones = suscripciones;
+    }
+
+    public Usuario getCreados() {
+        return creados;
+    }
+
+    public void setCreados(Usuario creados) {
+        this.creados = creados;
+    }
+
+    public List<Comentario> getComentarios() {
+        return comentarios;
+    }
+
+    public void setComentarios(List<Comentario> comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    public List<Usuario> getSeguidores() {
+        return seguidores;
+    }
+
+    public void setSeguidores(List<Usuario> seguidores) {
+        this.seguidores = seguidores;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lista lista = (Lista) o;
+        return id == lista.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Lista{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", dni_usuario='" + dni_usuario + '\'' +
+                ", suscripciones=" + suscripciones +
+                ", creados=" + creados +
+                ", comentarios=" + comentarios +
+                ", seguidores=" + seguidores +
+                '}';
+    }
 }
--- src/main/java/accesoadatos/soundwaveproject/model/Usuario.java
+++ src/main/java/accesoadatos/soundwaveproject/model/Usuario.java
@@ -1,4 +1,121 @@
 package accesoadatos.soundwaveproject.model;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 public class Usuario {
+    private String dni;
+    private String nombre;
+    private String correo;
+    private String contraseña;
+    private byte[] foto;
+    List<Lista> misListas;
+    List<Comentario> comentarios;
+    List<Lista> suscripciones;
+
+    public Usuario() {
+    }
+
+    public Usuario(String dni, String nombre, String correo, String contraseña, byte[] foto, List<Lista> misListas, List<Comentario> comentarios, List<Lista> suscripciones) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contraseña = contraseña;
+        this.foto = foto;
+        this.misListas = misListas;
+        this.comentarios = comentarios;
+        this.suscripciones = suscripciones;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public byte[] getFoto() {
+        return foto;
+    }
+
+    public void setFoto(byte[] foto) {
+        this.foto = foto;
+    }
+
+    public List<Lista> getMisListas() {
+        return misListas;
+    }
+
+    public void setMisListas(List<Lista> misListas) {
+        this.misListas = misListas;
+    }
+
+    public List<Comentario> getComentarios() {
+        return comentarios;
+    }
+
+    public void setComentarios(List<Comentario> comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    public List<Lista> getSuscripciones() {
+        return suscripciones;
+    }
+
+    public void setSuscripciones(List<Lista> suscripciones) {
+        this.suscripciones = suscripciones;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(dni, usuario.dni) && Objects.equals(correo, usuario.correo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dni, correo);
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "dni='" + dni + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", correo='" + correo + '\'' +
+                ", contraseña='" + contraseña + '\'' +
+                ", foto=" + Arrays.toString(foto) +
+                ", misListas=" + misListas +
+                ", comentarios=" + comentarios +
+                ", suscripciones=" + suscripciones +
+                '}';
+    }
 }
